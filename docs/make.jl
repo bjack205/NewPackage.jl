2f9b7ba3ac0a9e0a8f295eb377d09553fa2afaee
Brian Jackson <bjack205@gmail.com>
2021-09-17 07:09:40 -0400

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter
+using NewPackage
+
+makedocs(
+    sitename = "NewPackage.jl",
+    format = Documenter.HTML(prettyurls = false),
+    pages = [
+        "Introduction" => "index.md",
+        "API" => "api.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/bjack205/NewPackage.jl.git",
+    devbranch = "main"
+)
---
+++ docs/src/index.md
@@ -0,0 +1,4 @@
+# NewPackage.jl  
+ 
+## Overview 
+This package is part of a tutorial by Brian Jackson on setting up a new Julia package.
